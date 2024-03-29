9870b1e9e54ef7926fcfbf23166d54638db62c54
accountbelongstox <accountbelongstox@163.com>
2024-03-30 00:58:53 +0800

    2024-03-30 00:57:32

--- gitput.bat
+++ gitput.bat
@@ -9,11 +9,26 @@ set "hour=%datetime:~8,2%"
 set "minute=%datetime:~10,2%"
 set "second=%datetime:~12,2%"
 set "timestamp=%year%-%month%-%day% %hour%:%minute%:%second%"
-
+set "core_node_dir=%~dp0core_node\"
 
 REM git
 git add .
 git commit -m "%timestamp%"
 git push --set-upstream origin main
+
+REM Check if the directory exists and navigate into it
+if exist "%core_node_dir%" (
+    echo Entering %core_node_dir%
+    cd /d "%core_node_dir%"
+    
+    REM Now within the core_node directory, execute Git commands again
+    git add .
+    git commit -m "%timestamp%"
+    git push --set-upstream origin main
+    
+    echo Exiting %core_node_dir%
+)
+
+echo Current working directory: %cd%
 
 endlocal
